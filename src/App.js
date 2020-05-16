a64af15d8908e4cc14e55e881362b1e4cf080487
FrankieBoye <frankie4@btinternet.com>
2020-05-16 21:39:37 +0100

    capacity state moved to app js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import Cylinders from './Cylinders'
 import Capacity from './Capacity'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleCapacityChange = this.handleCapacityChange.bind(this);
+    this.state = {
+      capacity: 2
+    }
+  }
+
+  handleCapacityChange(capacity){
+    this.setState({capacity})
+  }
+
   render() {
+    // const {capacity} = this.state
     return (
       <div className="App">
         <h1>Engine power</h1>
@@ -20,7 +33,10 @@ class App extends Component {
         </br>
         <br>
         </br>
-        <Capacity/>
+        <Capacity
+        // capacity={capacity}
+        capacity={this.state.capacity}
+        onCapacityChange={this.handleCapacityChange} />
       </div>
     )
   }
--- src/Capacity.js
+++ src/Capacity.js
@@ -3,24 +3,20 @@ import React, { Component } from 'react'
 class Capacity extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      capacity: 2
-    };
   }
 
   onInput() {
     var input = document.getElementById("capinp");
     var cap = input.value;
-    this.setState({
-      capacity: parseFloat(cap)
-    })
+    this.props.onCapacityChange(parseFloat(cap))
 }
 
   render() {
+  const {capacity} = this.props;
    return (
      <form name="capacity">
      <input id="capinp" type="range" min="1" max="7" step="0.1" defaultValue="2" onInput={this.onInput.bind(this)}/>
-     {this.state.capacity} litres
+     {capacity} litres
      </form>
   )
  }
